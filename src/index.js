7c7a412ce0c3f073297459aee3bac6151df3fcbc
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-06-02 22:20:10 +0300

    Initialize server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-console.log('Hello world');
 
+const express = require('express');
+
+const port = 3000;
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Home page');
+});
+
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
