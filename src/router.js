5e3752f3803fd426a3a6a743c4deca2677399ada
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-06-06 23:33:01 +0300

    Implement home controller

--- src/router.js
+++ src/router.js
@@ -1,10 +1,14 @@
 const express = require('express');
+
 const todoItemController = require('./controllers/todoItemController');
 const todoListController = require('./controllers/todoListController');
+const homeController = require('./controllers/homeController.js');
 
 const router = express.Router();
 
 router.use(todoItemController);
 router.use(todoListController);
+router.use(homeController);
+
 
 module.exports = router;
