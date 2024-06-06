1151e679b7b04353ff449b89381b0e223b0391b1
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-06-06 23:57:05 +0300

    Implement todoList controller and change todo Item controller

--- src/controllers/todoItemController.js
+++ src/controllers/todoItemController.js
@@ -3,4 +3,20 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('todoList/:todoListId/todoItem/:id', (req, res) => {
+
+    let todoListId = req.params.todoListId;
+    let todoItemId = req.params.id;
+
+    res.render('todoItem');
+});
+
+
+router.post('/todoList/:todoListId/todoItem', (req, res) => {
+
+    let todoListId = req.params.todoListId;
+
+});
+
+
 module.exports = router;
--- src/controllers/todoListController.js
+++ src/controllers/todoListController.js
@@ -3,4 +3,21 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('/todoList/all', (req, res) => {
+
+    res.render('todoListAll');
+})
+
+router.get('/todoList/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    res.render('todoList');
+});
+
+
+router.post('/todoList', (req, res) => {
+
+});
+
 module.exports = router;
